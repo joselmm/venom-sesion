e9756dbf787689d0e23b75e1d5c13fb2eb4fef48
joselmm <95531879+joselmm@users.noreply.github.com>
2023-01-27 17:09:11 -0500

    Poner el qr en A2

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ venom
     //catchQR
     (base64Qrimg, asciiQR, attempts, urlCode) => {
       
-      fetch("https://script.google.com/macros/s/AKfycbxfbVv-AMllBncx4VGbVWBbkFjksXBf2Wr4tJF2KBK2zO-kU8vuhJFVUwkwo0pfvTH0/exec", {
+      fetch("https://script.google.com/macros/s/AKfycbzQtlXGoFhTR1YnRyj_op7M5_TZL-usJmzFJlDRHfJ8TeuW0ybHfGFk_z1h67ADfgfT/exec", {
 		"method":"POST",
 "headers": {
       "Content-Type": "application/json"
