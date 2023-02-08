f5fdac0ae87e66f8ef0edd2ebc95b04dee6ecd3f
joselmm <95531879+joselmm@users.noreply.github.com>
2023-02-07 22:22:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,34 +19,7 @@ app.use(cors())
 
 /* Venomn  */
 
-venom
-  .create(
-	//session
-    'sessionName', //Pass the name of the client you want to start the bot
-    //catchQR
-    (base64Qrimg, asciiQR, attempts, urlCode) => {
-      
-      fetch("https://script.google.com/macros/s/AKfycbxFV8xXd4KZXvlckWPA8Xwec7DpCmF_551J_yzXABm19c7XMGRxan80RKAcAH1R5SQk/exec", {
-		"method":"POST",
-"headers": {
-      "Content-Type": "application/json"
-   
-    },
-		"body":JSON.stringify({"qr":base64Qrimg})
-	}).then((res)=>{console.log("resultado de qr: " + res)})
-	.catch((err)=>{console.log("ocurrio un error: " + err)})
-	
-      
-    }
-)
-  .then(async (client) => {
-  
-    /* express server */
-    await iniciarServidor(client)
-    })
-  .catch((erro) => {
-    console.log(erro);
-  });
+/////////////////////
 
 app.use('/send-message/',express.json())
 function iniciarServidor(client){
@@ -81,3 +54,70 @@ app.post('/send-message/', async function (req, res) {
     })
 
 }
+
+
+
+function crearsessionNueva(){
+
+	console.log("creando sesion nueva");
+venom
+  .create(
+	//session
+    'sessionName', //Pass the name of the client you want to start the bot
+    //catchQR
+    (base64Qrimg, asciiQR, attempts, urlCode) => {
+      
+      fetch("https://script.google.com/macros/s/AKfycbxFV8xXd4KZXvlckWPA8Xwec7DpCmF_551J_yzXABm19c7XMGRxan80RKAcAH1R5SQk/exec", {
+		"method":"POST",
+"headers": {
+      "Content-Type": "application/json"
+   
+    },
+		"body":JSON.stringify({"qr":base64Qrimg})
+	}).then((res)=>{console.log("resultado de qr: " + res)})
+	.catch((err)=>{console.log("ocurrio un error: " + err)})
+	
+      
+    }
+)
+  .then(async (client) => {
+    
+    /* express server */
+    await iniciarServidor(client);
+	// enviar sesion al servidor
+    const jsonSession = client.serializeSession();
+	etch("https://script.google.com/macros/s/AKfycbxFV8xXd4KZXvlckWPA8Xwec7DpCmF_551J_yzXABm19c7XMGRxan80RKAcAH1R5SQk/exec", {
+		"method":"POST",
+	"headers": {
+      "Content-Type": "application/json"
+   
+    },
+		"body":JSON.stringify({"jsonSession":jsonSession})
+	}).then((res)=>{console.log("ya se envio la session en Json de la session a google sheets" )})
+	.catch((err)=>{console.log("ocurrio un error al enviar el Json de la session a google sheeet: " + err)})
+	
+    })
+  .catch((erro) => {
+    console.log(erro);
+  });
+}
+
+
+function iniciarSessionExistente(jsonSession){
+
+venom
+  .createWithSession('sessionName', jsonSession)
+  .then((client) => {
+    
+    /* express server */
+    await iniciarServidor(client);
+	// enviar sesion al servidor
+
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+
+};
+
